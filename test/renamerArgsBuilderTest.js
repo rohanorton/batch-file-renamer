a0cd48010c54c167937c29ceebc2e1cfd176aa3f
Rohan Orton <rohan.orton@gmail.com>
2016-02-27 22:56:15 +0000

    Remove coffeescript tests
    They were failing in weird ways. Instead of assertions providing useful
    information, they were instead returning ENOENT errors for the testfile
    itself. This is to do with using fs-mock. Simplest solution was to just
    write the tests in javascript and abandon coffeescript altogether

---
+++ test/renamerArgsBuilderTest.js
@@ -0,0 +1,39 @@
+/*globals describe, it */
+import assert from 'assert';
+import renamerArgsBuilder from '../src/renamerArgsBuilder';
+
+describe('renamerArgsBuilder', () => {
+    it('zips together argument arrays', () => {
+        const actual = renamerArgsBuilder([ 'old1', 'old2', 'old3' ], [ 'new1', 'new2', 'new3']);
+        const expected = [ [ 'old1', 'new1' ], [ 'old2', 'new2' ], [ 'old3', 'new3' ] ];
+        assert.deepEqual(actual, expected);
+    });
+
+    it('removes duplication in pairs', () => {
+        const actual = renamerArgsBuilder([ 'old1', 'old2', 'old3' ], [ 'old1', 'old2', 'new3']);
+        const expected = [ [ 'old3', 'new3' ] ];
+        assert.deepEqual(actual, expected);
+    });
+
+    it('removes duplicate elements', () => {
+        const actual = renamerArgsBuilder([ 'old1', 'old1', 'old1' ], [ 'new1', 'new1', 'new1']);
+        const expected = [ [ 'old1', 'new1' ] ];
+        assert.deepEqual(actual, expected);
+    });
+
+    // Asserts:
+    it('throws error if source array unspecified', () => {
+        const fn = () => renamerArgsBuilder(null, [ 'new1' ]);
+        assert.throws(fn, /sources/i);
+    });
+
+    it('throws error if destination array unspecified', () => {
+        const fn = () => renamerArgsBuilder([ 'old1' ]);
+        assert.throws(fn, /destination/i);
+    });
+
+    it('throws error if array lengths mismatched', () => {
+        const fn = () => renamerArgsBuilder([ 'old1', 'old2', 'old3' ], [ 'new1' ]);
+        assert.throws(fn, /must match/i);
+    });
+});
