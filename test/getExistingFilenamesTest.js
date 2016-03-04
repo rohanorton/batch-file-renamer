f7d74fa00399f380109871f81c2bbcd1371af7de
Rohan Orton <rohan.orton@gmail.com>
2016-03-04 19:43:30 +0000

    Rewrite getExistingFilenamesTest to use chai promises

--- test/getExistingFilenamesTest.js
+++ test/getExistingFilenamesTest.js
@@ -1,7 +1,11 @@
 /*globals it, describe, beforeEach, afterEach */
 import mock from 'mock-fs';
-import assert from 'assert';
+import chai, { assert } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
 import getExistingFilenames from '../src/getExistingFilenames';
+
+chai.use(chaiAsPromised);
 
 const testDirectory = {
     testfile1: 'content of testfile1',
@@ -21,29 +25,19 @@ describe('getExistingFilenames', () => {
     it('should callback with array of all files if all source files exist', () => {
         const files = [ 'testfile1', 'testfile2', 'testfile3' ];
         const expected = files;
-        return getExistingFilenames(files)
-            .then(actual => {
-                assert.deepEqual(actual, expected);
-            });
+        const promise = getExistingFilenames(files);
+        return assert.becomes(promise, expected);
     });
     it('should error if files do not exist', () => {
         const files = [ 'testfile1', 'this-is-made-up', 'testfile2', 'testfile3', 'oops-another-made-up-file' ];
-        return getExistingFilenames(files)
-            .then(() => {
-                throw 'Should not return files';
-            }).catch(err => {
-                assert(err, 'Should error');
-            });
+        const promise = getExistingFilenames(files);
+        return assert.isRejected(promise);
     });
     it('should not error if file does not exist and passed { "ignore-missing-file": true } option', () => {
         const files = [ 'testfile1', 'this-is-made-up', 'testfile2', 'testfile3', 'oops-another-made-up-file' ];
         const expected = [ 'testfile1', 'testfile2', 'testfile3' ];
         const options = { 'ignore-missing-file': true };
-        return getExistingFilenames(files, options)
-            .then(actual => {
-                assert.deepEqual(actual, expected);
-            }).catch(err => {
-                assert(!err, 'Should not error');
-            });
+        const promise = getExistingFilenames(files, options);
+        return assert.becomes(promise, expected);
     });
 });
