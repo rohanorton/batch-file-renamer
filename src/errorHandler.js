eb7937d8b5a4744621a41532c1d666e3e143108b
Rohan Orton <rohan.orton@gmail.com>
2016-03-09 00:23:57 +0000

    Add custom error messages to error handler

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,14 +1,12 @@
 import chalk from 'chalk';
-import { get, includes } from 'lodash';
+import { get, includes, defaults } from 'lodash';
 import {DEBUG} from './flags';
 
-const formatMessage = (err) => {
-    switch (err.code) {
-    case 'ENOENT':
-        return 'No such file or directory: ' + err.path;
-    default:
-        return err.message;
-    }
+const formatMessage = (err, options = {}) => {
+    const codes = defaults({
+        ENOENT: 'No such file or directory: ' + err.path
+    }, args.errorMessages);
+    return codes[err.code] || err.message;
 }
 
 const errorHandler = (args = {}) => {
