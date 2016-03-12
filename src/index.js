e1ea62d677779a89d7ffe0d4538e1e3a89452dd4
Rohan Orton <rohan.orton@gmail.com>
2016-03-12 22:52:01 +0000

    Ensure exit on completion of renamer

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import batchFileRenamer from './batchFileRenamer';
 import errorHandler from './errorHandler';
 
 export default (args) =>
-    batchFileRenamer(args).catch(errorHandler(args));
+    batchFileRenamer(args)
+        .then(() => process.exit())
+        .catch(errorHandler(args));
