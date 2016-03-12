f3566c17f37eec9bbc2b8b3f012692baba32dd8e
Rohan Orton <rohan.orton@gmail.com>
2016-03-12 23:10:43 +0000

    Add explanatory comment to mockPrompter

--- test/utils/mockPrompter.js
+++ test/utils/mockPrompter.js
@@ -1,3 +1,6 @@
+// This prompter acts as a super simple drop-in replacement for the
+// prompter.prompt method provided by keypress-prompt.
+
 const mockPrompter = (...responses) =>
     () => responses.shift();
 
