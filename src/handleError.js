951a44fd84800844fe386944fe01ad1905234bfb
Rohan Orton <rohan.orton@gmail.com>
2016-03-26 12:07:29 +0000

    Fix error handler

--- src/handleError.js
+++ src/handleError.js
@@ -1,21 +1,23 @@
 import chalk from 'chalk';
-import { includes, defaults, noop } from 'lodash/fp';
+import { includes, isFunction, defaults, noop } from 'lodash/fp';
 import {DEBUG} from './flags';
 import logger from './logger';
 
 const formatMessage = (err, args = {}) => {
-    const codes = defaults({
-        ENOENT: `No such file or directory: '${err.path}'`,
-        EDUPLDEST: `Duplicate file destinations: '${ err.duplicates.join("', '")}'`
-    }, args.errorMessages);
-    return codes[err.code] || err.message || 'An unexpected error occurred';
+    const codes = defaults(args.errorMessages, {
+        ENOENT: () => `No such file or directory: '${err.path}'`,
+        EDUPLDEST: () =>`Duplicate file destinations: '${ err.duplicates.join("', '")}'`
+    });
+    return (codes[err.code] && isFunction(codes[err.code]) ? codes[err.code]() : codes[err.code]) ||
+        err.message ||
+        'An unexpected error occurred';
 }
 
 const handleError = (args = {}) => {
     return (err) => {
         const onError = args.onError || noop;
         onError();
-        logger.error('Error:')
+        logger.error('Error:');
         logger.error(formatMessage(err, args));
         logger.debug('');
         logger.debug(err.stack);
